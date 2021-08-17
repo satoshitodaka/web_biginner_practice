a6ca2070300edffaa7021908b9d9197e2a25df8a
satoshitodaka <satoshitodaka0705@gmail.com>
2021-08-18 07:02:28 +0900

    Time.nowの表示機能

--- webrick.rb
+++ webrick.rb
@@ -7,4 +7,12 @@ server = WEBrick::HTTPServer.new({
   :Port => 8000
 })
 
+server.mount_proc('/time') do |req, res|
+  # レスポンス内容を出力
+  body = "<html><body>#{Time.now}</body></html>"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
 server.start
