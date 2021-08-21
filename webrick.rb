d9e3695394e984815f8ad62837c865f88c93fbd2
satoshitodaka <satoshitodaka0705@gmail.com>
2021-08-21 10:01:50 +0900

    finish form practice

--- webrick.rb
+++ webrick.rb
@@ -1,5 +1,6 @@
  # webrick.rb
 require 'webrick'
+require 'webrick/cgi'
 
 server = WEBrick::HTTPServer.new({ 
   :DocumentRoot => './',
@@ -10,6 +11,22 @@ server = WEBrick::HTTPServer.new({
 server.mount_proc('/time') do |req, res|
   # レスポンス内容を出力
   body = "<html><body>#{Time.now}</body></html>"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
+server.mount_proc('/form_get') do |req, res|
+  data = req.query
+  body = "<html><head><meta charset='utf-8'></head><body>クエリパラメータは#{data}です。<br>こんにちは#{data['username']}さん。あなたの年齢は#{data['age']}歳ですね。</body></html>"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
+server.mount_proc('/form_post') do | req, res|
+  data = req.query
+  body = "<html><head><meta charset='utf-8'></head><body>クエリパラメータは#{data}です。<br>こんにちは#{data['username']}さん。あなたの年齢は#{data['age']}歳ですね。</body></html>"
   res.status = 200
   res['Content-Type'] = 'text/html'
   res.body = body
