d929cc2b213067fdf5cd102c32fb66897fb25c88
satoshitodaka <satoshitodaka0705@gmail.com>
2021-08-22 14:23:28 +0900

    force_encodingを追加し、ひらがなの入力に対応した。

--- webrick.rb
+++ webrick.rb
@@ -18,7 +18,11 @@ end
 
 server.mount_proc('/form_get') do |req, res|
   data = req.query
-  body = "<html><head><meta charset='utf-8'></head><body>クエリパラメータは#{data}です。<br>こんにちは#{data['username']}さん。あなたの年齢は#{data['age']}歳ですね。</body></html>"
+  name = req.query['user_name']
+  age = req.query['age']
+  body = "<html><head><meta charset='utf-8'></head><body>\n"
+  body += "クエリパラメータは#{data}です。<br>こんにちは#{name.force_encoding('utf-8')}さん。あなたの年齢は#{age.to_s}歳ですね。"
+  body += "</body></html>"
   res.status = 200
   res['Content-Type'] = 'text/html'
   res.body = body
@@ -26,7 +30,12 @@ end
 
 server.mount_proc('/form_post') do | req, res|
   data = req.query
-  body = "<html><head><meta charset='utf-8'></head><body>フォームデータは#{data}です。<br>こんにちは#{data['username']}さん。あなたの年齢は#{data['age']}歳ですね。</body></html>"
+  name = req.query['user_name']
+  age = req.query['age']
+  body = "<html><head><meta charset='utf-8'></head><body>\n"
+  body += "フォームデータは#{data}です。<br>こんにちは#{name.force_encoding('utf-8')}さん。あなたの年齢は#{age}歳ですね。"
+  # to_sは使って何か変わるか？
+  body += "</body></html>"
   res.status = 200
   res['Content-Type'] = 'text/html'
   res.body = body
